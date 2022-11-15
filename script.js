c7dac9b47d1722fc0dbdfa1f04b09863bd32da2c
JoseNunhez <nunhezmart@gmail.com>
2022-11-15 13:36:28 +0100

    Añado generador de palabra aleatoria, y bloqueo de botones

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Initial References
+s/* Initial References
 const letterContainer = document.getElementById("letter-container");
 const optionsContainer = document.getElementById("options-container");
 const userInputSection = document.getElementById("user-input-section");
@@ -39,7 +39,51 @@ const mostrarOpciones = () => {
     contenedorOpcionesJugador.innerHTML += `<h3> Selecciona una opcion </h3>`;
   let buttonPalabrasAhorcado = document.createElement("div");
   for (let value in hangmanWords) {
-    buttonPalabrasAhorcado.innerHTML += `<button class="buttonPalabrasAhorcado" onclick="generateWord('${value}')">${value}</button>`;
+    buttonPalabrasAhorcado.innerHTML += `<button class="opciones" onclick="generateWord('${value}')">${value}</button>`;
   }
   contenedorOpcionesJugador.appendChild(buttonPalabrasAhorcado);
+};
+
+//Bloquear todos los botones
+const bloquear = () => {
+    let opcionesButtons = document.querySelectorAll(".opciones");
+    let letrasButtons = document.querySelectorAll(".letters");
+    //Desabilitar Opciones
+    opcionesButtons.forEach((button) => {
+      button.disabled = true;
+    });
+  
+    //Deshabilitar letras
+    letrasButtons.forEach((button) => {
+      button.disabled.true;
+    });
+    contendorJuegoNuevo.classList.remove("hide");
+  };
+
+//Generador de palabra
+
+const generarPalabra = (palabrasAhorcadoValue) => {
+    let buttonOpciones = document.querySelectorAll(".opciones");
+    //If optionValur matches the button innerText then highlight the button
+    buttonOpciones.forEach((button) => {
+        if (button.innerText.toLowerCase() === palabrasAhorcadoValue){
+            button.classList.add("activo");
+        }
+        button.disabled = true;
+    })
+
+//Ocultar letras, y limpiar palabra anterior
+contenedorLetras.classList.remove("hide");
+sectionInputJugador.innerText = "";
+
+let opcionArray = palabrasAhorcado[palabrasAhorcadoValue];
+//Escoger Palabra Random
+palabraElegida = opcionArray[Math.floor(Math.random()* opcionArray.length)];
+palabraElegida = palabraElegida.toUpperCase();
+
+//Reemplazar cada letra con un "_"
+let palabraOculta = palabraElegida.replace(/./g, '<span class="dashes">_</span>')
+
+//Muestra cada elemento como un span
+sectionInputJugador.innerHTML = palabraOculta;
 };
