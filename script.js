a68fa23183dc8d4d96ab7a29803760d833098a70
JoseNunhez <nunhezmart@gmail.com>
2022-11-16 01:21:35 +0100

    Añado la letra Ñ en codigo ascii, y completo funcion iniciar

--- script.js
+++ script.js
@@ -48,51 +48,31 @@ const mostrarOpciones = () => {
 
 
 //funcion inicial para cuando la pagina carga o el usuario presione ara juego nuevo
-const initializer = () => {
+const iniciar = () => {
   winCount = 0;
   count = 0;
-  mostrarOpciones();
-};
-window.onload = initializer;
 
-
-//iniciar un juego nuevo
-buttonJuegoNuevo.addEventListener("click", initializer);
-window.onload = initializer;
-
-
-//Bloquear todos los botones
-const bloquear = () => {
-  let opcionesButtons = document.querySelectorAll(".opciones");
-  let letrasButtons = document.querySelectorAll(".letters");
-  //Desabilitar todas las opciones
-  opcionesButtons.forEach((button) => {
-    button.disabled = true;
-  });
-
-  //Deshabilitar letras
-  letrasButtons.forEach((button) => {
-    button.disabled.true;
-  });
-  contendorJuegoNuevo.classList.remove("hide");
-};
-
-
+  //Ocultamos todas las letras y el boton de juego nuevo
+  sectionInputJugador.innerHTML = "";
+  contenedorOpcionesJugador.innerHTML = "";
+  contenedorLetras.classList.add("hide");
+  contendorJuegoNuevo.classList.add("hide");
+  contenedorLetras.innerHTML = "";
 
 // abecedario dentro del contenedor
-for (let i = 65; i < 91; i++) {
+for (let i = 65; i < 210; i++) {
   let button = document.createElement("button");
   button.classList.add("letras");
   /// usa los números del 65 al 97 de acuerdo al la numeración en la lista ASCII (A-Z)
+  if ((i>64 && i<91) || i === 209){
   button.innerText = String.fromCharCode(i);
-  // Click del boton de letras
   button.addEventListener("click", () => {
     let letrasArray = palabraElegida.split("");
     let dashes = document.getElementsByClassName("dashes");
-  // Si el array de letras contiene la letra clicada se reemplaza el dash por la letra correspondiente( y si no se dibuja una parte del ahorcado)
+    // Si el array de letras contiene la letra clicada se reemplaza el dash por la letra correspondiente( y si no se dibuja una parte del ahorcado)
     if (letrasArray.includes(button.innerText)){
       letrasArray.forEach((char, index) => {
-      //Si la letra clicada está en el array
+        //Si la letra clicada está en el array
         if (char === button.innerText){
           //Reemplazo dash por letra e incrementamos contador
           dashes[index].innerText = char;
@@ -115,11 +95,35 @@ for (let i = 65; i < 91; i++) {
         bloquear();
       }
     }
-  //Desabilitar boton click
-  button.disabled = true;
+    //Desabilitar boton click
+    button.disabled = true;
   });
   contenedorLetras.append(button);
 }
+}
+mostrarOpciones();
+};
+
+//Bloquear todos los botones
+const bloquear = () => {
+  let opcionesButtons = document.querySelectorAll(".opciones");
+  let letrasButtons = document.querySelectorAll(".letters");
+  //Desabilitar todas las opciones
+  opcionesButtons.forEach((button) => {
+    button.disabled = true;
+  });
+  
+  //Deshabilitar letras
+  letrasButtons.forEach((button) => {
+    button.disabled.true;
+  });
+  contendorJuegoNuevo.classList.remove("hide");
+};
+
+
+
+
+
 
 
 
@@ -135,26 +139,30 @@ const generarPalabra = (palabrasAhorcadoValue) => {
     }
     button.disabled = true;
   });
-
+  
   //Ocultar letras, y limpiar palabra anterior
   contenedorLetras.classList.remove("hide");
   sectionInputJugador.innerText = "";
-
+  
   let opcionArray = palabrasAhorcado[palabrasAhorcadoValue];
   
   //Escoger Palabra Random
   palabraElegida = opcionArray[Math.floor(Math.random() * opcionArray.length)];
   palabraElegida = palabraElegida.toUpperCase();
   console.log(palabraElegida);
-
+  
   //Reemplazar cada letra con un "_"
   let palabraOculta = palabraElegida.replace(
     /./g,
     '<span class="dashes">_</span>&nbsp;' //&nbsp; pone espacio para separar cada letra y _
-  );
-
-  //Muestra cada elemento como un span
-  sectionInputJugador.innerHTML = palabraOculta;
-};
-
-
+    );
+    
+    //Muestra cada elemento como un span
+    sectionInputJugador.innerHTML = palabraOculta;
+  };
+  
+  
+  //iniciar un juego nuevo
+  buttonJuegoNuevo.addEventListener("click", iniciar);
+  window.onload = iniciar;
+  
