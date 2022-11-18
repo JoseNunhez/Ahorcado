32bdfc7eedd9d9a9f0e81451708eb74208162e2c
sara <saravillazana@gmail.com>
2022-11-19 00:09:00 +0100

    dark mode, nombre en img, css buttons, background

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Declaración variables
 
 const contenedorLetras = document.getElementById("contenedor-letras");
@@ -8,7 +7,9 @@ const contendorJuegoNuevo = document.getElementById("contenedor-juego-nuevo");
 const buttonJuegoNuevo = document.getElementById("button-juego-nuevo");
 const imagenesAhorcado = document.getElementById("imagenes-ahorcado");
 const resultadoTexto = document.getElementById("resultado-texto");
-const contenedorPuntuaciones = document.getElementById("puntuaciones-jugadores")
+const contenedorPuntuaciones = document.getElementById(
+  "puntuaciones-jugadores"
+);
 const inputUsuario = document.getElementById("usuario");
 let mejoresPuntuaciones = [];
 let nombreUsuario = "-";
@@ -59,22 +60,28 @@ const bloquear = () => {
 const iniciar = () => {
   winCount = 0;
   count = 0;
-  if(nombreUsuario.length >1){cambiarNombre()}
-  else{nombre()}
-  
+  if (nombreUsuario.length > 1) {
+    cambiarNombre();
+  } else {
+    nombre();
+  }
 
   //Mostramos mejores puntuaciones
-if (mejoresPuntuaciones.length > 0){
-  contenedorPuntuaciones.innerHTML = "<tr><th>NOMBRE</th><th>PUNTUACION</th></tr>";
-  for ( let i in mejoresPuntuaciones){
-    contenedorPuntuaciones.innerHTML += 
-    '<tr>'+
-    '<td>'+mejoresPuntuaciones[i]['name']+'</td>'+
-    '<td>'+mejoresPuntuaciones[i]['puntuacion']+'</td>'+
-    '</tr>';
+  if (mejoresPuntuaciones.length > 0) {
+    contenedorPuntuaciones.innerHTML =
+      "<tr><th>NOMBRE</th><th>PUNTUACION</th></tr>";
+    for (let i in mejoresPuntuaciones) {
+      contenedorPuntuaciones.innerHTML +=
+        "<tr>" +
+        "<td>" +
+        mejoresPuntuaciones[i]["name"] +
+        "</td>" +
+        "<td>" +
+        mejoresPuntuaciones[i]["puntuacion"] +
+        "</td>" +
+        "</tr>";
+    }
   }
-}
-
 
   //Ocultamos todas las letras y el boton de juego nuevo
   sectionInputJugador.innerHTML = "";
@@ -82,93 +89,104 @@ if (mejoresPuntuaciones.length > 0){
   contenedorLetras.classList.add("hide");
   contendorJuegoNuevo.classList.add("hide");
   contenedorLetras.innerHTML = "";
-  imagenesAhorcado.innerHTML = ""
+  imagenesAhorcado.innerHTML = "";
   let a = true;
-// abecedario dentro del contenedor
-for (let i = 65; i <92; i++) {
-  let button = document.createElement("button");
-  button.classList.add("letras");
-  /// usa los números del 65 al 97 de acuerdo al la numeración en la lista ASCII (A-Z)
-  if ((i>64 && i<91) || i === 209){
-    if(i===79 && a){
-      i=209;
-      a=false;
-    }
-    button.innerText = String.fromCharCode(i);
-    button.addEventListener("click", () => {
-      let letrasArray = palabraElegida.split("");
-      let dashes = document.getElementsByClassName("dashes");
-      // Si el array de letras contiene la letra clicada se reemplaza el dash por la letra correspondiente( y si no se dibuja una parte del ahorcado)
-      if (letrasArray.includes(button.innerText)){
-        letrasArray.forEach((char, index) => {
-          //Si la letra clicada está en el array
-          if (char === button.innerText){
-            //Reemplazo dash por letra e incrementamos contador
-            dashes[index].innerText = char;
-            winCount += 1;
-            // si el wincount es igual a la longitud de la palabra ganas
-            if (winCount === letrasArray.length) {
-              puntuacionFinal = (10-count)*palabraElegida.length
-              mejoresPuntuaciones.push({name: nombreUsuario, puntuacion: puntuacionFinal})
-              suprimirPuntuaciones(mejoresPuntuaciones);
-              console.log(mejoresPuntuaciones)
-              resultadoTexto.innerHTML = `<h2 class='mensaje-ganador'>¡HAS GANADO! Puntuacion de: ${puntuacionFinal}</h2><p>La palabra era <span>${palabraElegida}</span></p>`;
-              //Bloquear todos los botones
-              bloquear();
+  // abecedario dentro del contenedor
+  for (let i = 65; i < 92; i++) {
+    let button = document.createElement("button");
+    button.classList.add("letras");
+    /// usa los números del 65 al 97 de acuerdo al la numeración en la lista ASCII (A-Z)
+    if ((i > 64 && i < 91) || i === 209) {
+      if (i === 79 && a) {
+        i = 209;
+        a = false;
+      }
+      button.innerText = String.fromCharCode(i);
+      button.addEventListener("click", () => {
+        let letrasArray = palabraElegida.split("");
+        let dashes = document.getElementsByClassName("dashes");
+        // Si el array de letras contiene la letra clicada se reemplaza el dash por la letra correspondiente( y si no se dibuja una parte del ahorcado)
+        if (letrasArray.includes(button.innerText)) {
+          letrasArray.forEach((char, index) => {
+            //Si la letra clicada está en el array
+            if (char === button.innerText) {
+              //Reemplazo dash por letra e incrementamos contador
+              dashes[index].innerText = char;
+              winCount += 1;
+              // si el wincount es igual a la longitud de la palabra ganas
+              if (winCount === letrasArray.length) {
+                puntuacionFinal = (10 - count) * palabraElegida.length;
+                mejoresPuntuaciones.push({
+                  name: nombreUsuario,
+                  puntuacion: puntuacionFinal,
+                });
+                suprimirPuntuaciones(mejoresPuntuaciones);
+                console.log(mejoresPuntuaciones);
+                resultadoTexto.innerHTML = `<h2 class='mensaje-ganador'>¡HAS GANADO! Puntuacion de: ${puntuacionFinal}</h2><p>La palabra era <span>${palabraElegida}</span></p>`;
+                //Bloquear todos los botones
+                bloquear();
+              }
             }
+          });
+        } else {
+          //Contador de oportunidades (y aladir imagen del ahorcado)
+          count += 1;
+          switch (count) {
+            case 1:
+              imagenesAhorcado.innerHTML = "";
+              imagenesAhorcado.innerHTML +=
+                '<img src="Imagenes/Imagen1.png" alt="">';
+              break;
+            case 2:
+              imagenesAhorcado.innerHTML = "";
+              imagenesAhorcado.innerHTML +=
+                '<img src="Imagenes/Imagen2.png" alt="">';
+              break;
+            case 3:
+              imagenesAhorcado.innerHTML = "";
+              imagenesAhorcado.innerHTML +=
+                '<img src="Imagenes/Imagen3.png" alt="">';
+              break;
+            case 4:
+              imagenesAhorcado.innerHTML = "";
+              imagenesAhorcado.innerHTML +=
+                '<img src="Imagenes/Imagen4.png" alt="">';
+              break;
+            case 5:
+              imagenesAhorcado.innerHTML = "";
+              imagenesAhorcado.innerHTML +=
+                '<img src="Imagenes/Imagen5.png" alt="">';
+              break;
+            case 6:
+              imagenesAhorcado.innerHTML = "";
+              imagenesAhorcado.innerHTML +=
+                '<img src="Imagenes/Imagen6.png" alt="">';
+              break;
           }
-        });
-    }
-    else {
-      //Contador de oportunidades (y aladir imagen del ahorcado)
-      count += 1;
-      switch(count){
-        case 1:
-          imagenesAhorcado.innerHTML = ""
-          imagenesAhorcado.innerHTML +=  '<img src="Imagenes/Imagen1.png" alt="">'
-          break;
-        case 2:
-          imagenesAhorcado.innerHTML = ""
-          imagenesAhorcado.innerHTML +=  '<img src="Imagenes/Imagen2.png" alt="">'
-          break;
-        case 3:
-          imagenesAhorcado.innerHTML = ""
-          imagenesAhorcado.innerHTML +=  '<img src="Imagenes/Imagen3.png" alt="">'
-          break;
-        case 4:
-          imagenesAhorcado.innerHTML = ""
-          imagenesAhorcado.innerHTML +=  '<img src="Imagenes/Imagen4.png" alt="">'
-          break;
-        case 5:
-          imagenesAhorcado.innerHTML = ""
-          imagenesAhorcado.innerHTML +=  '<img src="Imagenes/Imagen5.png" alt="">'
-          break;
-        case 6:
-          imagenesAhorcado.innerHTML = ""
-          imagenesAhorcado.innerHTML +=  '<img src="Imagenes/Imagen6.png" alt="">'
-          break;
-      }
-      //Si el contador es igual a 6, el jugador pierde
-      if(count===7){
-        puntuacionFinal = palabraElegida.length+winCount
-        mejoresPuntuaciones.push({name: nombreUsuario, puntuacion: puntuacionFinal})
-        suprimirPuntuaciones(mejoresPuntuaciones);
-        console.log(mejoresPuntuaciones)
-        resultadoTexto.innerHTML = `<h2 class='mensaje-perdedor'>¡HAS PERDIDO! Puntuacion de: ${puntuacionFinal}</h2><p>La palabra era <span>${palabraElegida}</span></p>`;
-        bloquear();
+          //Si el contador es igual a 6, el jugador pierde
+          if (count === 7) {
+            puntuacionFinal = palabraElegida.length + winCount;
+            mejoresPuntuaciones.push({
+              name: nombreUsuario,
+              puntuacion: puntuacionFinal,
+            });
+            suprimirPuntuaciones(mejoresPuntuaciones);
+            console.log(mejoresPuntuaciones);
+            resultadoTexto.innerHTML = `<h2 class='mensaje-perdedor'>¡HAS PERDIDO! Puntuacion de: ${puntuacionFinal}</h2><p>La palabra era <span>${palabraElegida}</span></p>`;
+            bloquear();
+          }
+        }
+        //Desabilitar boton click
+        button.disabled = true;
+      });
+      contenedorLetras.append(button);
+      if (i === 209) {
+        i = 78;
       }
     }
-    //Desabilitar boton click
-    button.disabled = true;
-  });
-  contenedorLetras.append(button);
-  if(i===209){i=78}
-}
-}
-mostrarOpciones();
+  }
+  mostrarOpciones();
 };
-
-
 
 //Generador de palabras
 
@@ -209,21 +227,32 @@ buttonJuegoNuevo.addEventListener("click", iniciar);
 window.onload = iniciar;
 
 //Sistema de puntuaciones
-function suprimirPuntuaciones(array){
+function suprimirPuntuaciones(array) {
   array.sort((a, b) => b.puntuacion - a.puntuacion);
-  if(array.length > 3){
+  if (array.length > 3) {
     array.pop();
-    return array
+    return array;
   }
 }
-function nombre(){
-  inputUsuario.innerHTML = "<p>Introduzca su nombre:</p><input class='nombre' type='text' id='inputNombre'><button class='nombre' onclick='guardarNombre()'>GUARDAR</button> ";
+function nombre() {
+  inputUsuario.innerHTML =
+    "<p>Introduzca su nombre:</p><input class='nombre' type='text' id='inputNombre'><button class='nombre' onclick='guardarNombre()'>GUARDAR</button> ";
 }
-function guardarNombre(){
+function guardarNombre() {
   nombreUsuario = document.getElementById("inputNombre").value;
-  inputUsuario.innerHTML = "<button class='nombre'onclick='nombre()'>CAMBIAR NOMBRE</button>"
+  inputUsuario.innerHTML =
+    "<button class='nombre'onclick='nombre()'>CAMBIAR NOMBRE</button>";
 }
-function cambiarNombre(){
-  inputUsuario.innerHTML = "<button class='nombre' onclick='nombre()'>CAMBIAR NOMBRE</button>"
-  console.log(nombreUsuario)
+function cambiarNombre() {
+  inputUsuario.innerHTML =
+    "<button class='nombre' onclick='nombre()'>CAMBIAR NOMBRE</button>";
+  console.log(nombreUsuario);
 }
+
+//modo nocturno
+document.getElementById("modo-dia").onclick = function () {
+  document.body.classList.remove("modo-nocturno");
+};
+document.getElementById("modo-noche").onclick = function () {
+  document.body.classList.add("modo-nocturno");
+};
